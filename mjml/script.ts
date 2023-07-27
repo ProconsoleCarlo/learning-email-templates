51843eb78906cff8418cc391920c6d9d47e7be62
Carlo Bobba <carlo.bobba@outlook.it>
2023-07-27 23:21:06 +0200

    Partials doesn't make any sense!

--- mjml/script.ts
+++ mjml/script.ts
@@ -1,7 +1,10 @@
 import mjml2html from 'mjml';
 import fs from 'fs';
+import { registerComponent } from 'mjml-core'
+import MjImageText from './lib/MjImageText'
 
 const mjml = fs.readFileSync('./templates/template.mjml', 'utf8');
+registerComponent(MjImageText)
 const htmlOutput = mjml2html(mjml);
 
 fs.writeFileSync('./build/output.html', htmlOutput.html);
